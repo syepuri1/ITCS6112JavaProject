2c9841604e4265577d8c714ef1fb480f278f5d57
Telsi Tamuzi <ttamuzi@uncc.edu>
2021-10-24 11:33:48 -0400

    Added CrossOrigin to user controller

--- src/main/java/com/event/management/controller/UsersController.java
+++ src/main/java/com/event/management/controller/UsersController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,7 @@ import com.event.management.service.UsersService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/users")
 @Api(value = "User", description = "User Apis", tags = { "User" })
